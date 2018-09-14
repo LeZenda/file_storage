002b9f915ef7eb8e47c0933108f167f5cf4587c4
Taras Datsiuk <taras.datsiuk@lifeisgoodlabs.com>
2018-09-14 09:27:36 +0300

    description fix

--- file_storage/files_app/tasks.py
+++ file_storage/files_app/tasks.py
@@ -8,9 +8,12 @@ from files_app.utils import search_documents
 from files_app.admin import DocumentResource
 
 
-# Function that saves documents to local storage
 @shared_task
 def export_documents(search_str):
+    """
+        This tasks receives search string, filters documents and stores them to local storage
+    """
+
     # small fix, since in queue empty string is converted to undefined
     search_str = search_str if search_str != 'undefined' else ''
 
